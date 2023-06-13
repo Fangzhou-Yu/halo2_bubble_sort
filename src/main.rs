1249b582ea66fa9d9bc164bda18260f01d934401
Fangzhou Yu <fangzhou.yu.25@dartmouth.edu>
2023-06-12 18:32:23 -0700

    I think all works besides one func

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ use std::{marker::PhantomData};
 
 use halo2_proofs::{
     arithmetic::FieldExt,
-    circuit::{AssignedCell, Layouter, SimpleFloorPlanner},
-    plonk::{Advice, Circuit, Column, ConstraintSystem, Error, Selector},
+    circuit::*,
+    plonk::*,
     poly::Rotation,
 };
 
@@ -17,6 +17,10 @@ struct BubSortConfig {
     b: Column<Advice>,
     c: Column<Advice>,
     d: Column<Advice>,
+    a_0 : Column<Instance>,
+    b_0 : Column<Instance>,
+    c_0 : Column<Instance>,
+    d_0 : Column<Instance>,
     s: Selector,
 }
 
@@ -42,6 +46,10 @@ impl<F: FieldExt> BubSortChip<F> {
         let b = meta.advice_column();
         let c = meta.advice_column();
         let d = meta.advice_column();
+        let a_0 = meta.instance_column();
+        let b_0 = meta.instance_column();
+        let c_0 = meta.instance_column();
+        let d_0 = meta.instance_column();
         let s = meta.selector();
 
         // enable permutation checks for the following columns
@@ -49,6 +57,10 @@ impl<F: FieldExt> BubSortChip<F> {
         meta.enable_equality(b);
         meta.enable_equality(c);
         meta.enable_equality(d);
+        meta.enable_equality(a_0);
+        meta.enable_equality(b_0);
+        meta.enable_equality(c_0);
+        meta.enable_equality(d_0);
 
         // define the custom gate
         meta.create_gate("swap", |meta| {
@@ -65,18 +77,14 @@ impl<F: FieldExt> BubSortChip<F> {
         });
 
         BubSortConfig {
-            a, b, c, d, s,
+            a, b, c, d, a_0, b_0, c_0, d_0, s,
         }
     }
 
     fn load_first_row(
         &self,
         mut layouter: impl Layouter<F>,
-        a: F,
-        b: F,
-        c: F,
-        d: F,
-    ) -> Result<(Number<F>, Number<F>, Number<F>, Number<F>), Error> {
+    ) -> Result<(AssignedCell<F,F>, AssignedCell<F,F>, AssignedCell<F,F>, AssignedCell<F,F>), Error> {
         // load first row
         layouter.assign_region(
             || "first row",
@@ -84,107 +92,111 @@ impl<F: FieldExt> BubSortChip<F> {
                 // enable the selector
                 self.config.s.enable(&mut region, 0)?;
 
-                let a_num = region.assign_advice(
+
+                let a_num = region.assign_advice_from_instance(
                     || "a",
-                    self.config.a, // column a
+                    self.config.a_0, // column a
                     0, // rotation
-                    || a,
-                ).map(Number)?;
-
-                let b_num = region.assign_advice(
+                    self.config.a,
+                    0,
+                )?;
+                
+                let b_num = region.assign_advice_from_instance(
                     || "b",
-                    self.config.b, // column b
-                    0, // rotation
-                    || b,
-                ).map(Number)?;
-
-                let c_num = region.assign_advice(
+                    self.config.b_0, // column b
+                    0, 
+                    self.config.b,
+                    0,
+                )?;
+                
+                let c_num = region.assign_advice_from_instance(
                     || "c",
-                    self.config.c, // column c
-                    0, // rotation
-                    || c,
-                ).map(Number)?;
+                    self.config.c_0, // column c
+                    0, 
+                    self.config.c,
+                    0,
+                )?;
 
-                let d_num = region.assign_advice(
+                let d_num = region.assign_advice_from_instance(
                     || "d",
-                    self.config.d, // column c
+                    self.config.d_0, // column c
                     0, // rotation
-                    || d,
-                ).map(Number)?;
+                    self.config.d,
+                    0,
+                )?;
 
                 Ok((a_num, b_num, c_num, d_num))
             },
         )
     }
 
+
+    /// JUST THIS FUCKING FUNC NOW
     fn load_row(
         &self,
         mut layouter: impl Layouter<F>,
-        prev_a: &Number<F>,
-        prev_b: &Number<F>,
-        prev_c: &Number<F>,
-        prev_d: &Number<F>,
-    ) -> Result<(Number<F>, Number<F>, Number<F>, Number<F>), Error> {
+        prev_a: &AssignedCell<F,F>,
+        prev_b: &AssignedCell<F,F>,
+        prev_c: &AssignedCell<F,F>,
+        prev_d: &AssignedCell<F,F>,
+    ) -> (AssignedCell<F,F>, AssignedCell<F,F>, AssignedCell<F,F>, AssignedCell<F,F>) {
         // do a compare & swap and assign a new region
         let mut arr = [prev_a.clone(), prev_b.clone(), prev_c.clone(), prev_d.clone()];
         for idx in 0..3 {
             let mut a = arr[idx];
             let mut b = arr[idx+1];
 
-            let val = a.0.value().and_then(|a| b.0.value().map(|b| *a > *b));
-
-            if val.as_ref().map(|v| *v == true) {
+            if a.value() > b.value() {
                 arr.swap(idx, idx+1)
             }
         }
 
+        /// DEBUG
 
         layouter.assign_region(
-            || "row",
+            || "next row",
             |mut region| {
                 // enable the selector
                 self.config.s.enable(&mut region, 0)?;
+
+                // Maybe jusy copy every value over instead?
                 
-                let a_val = arr[0].0.value();
-                let b_val = arr[1].0.value();
-                let c_val = arr[2].0.value();
-                let d_val = arr[3].0.value();
 
                 // a
-                region.assign_advice(
+                let a_cell = region.assign_advice(
                     || "a",
                     self.config.a,
                     0,
-                    || a_val.map(|a_val| a_val).ok_or(Error::Synthesis),
-                ).map(Number);
+                    || arr[0].value().copied(),
+                )?;
 
                 // b
-                region.assign_advice(
+                let b_cell = region.assign_advice(
                     || "b",
                     self.config.b,
                     0,
-                    || b_val.ok_or(Error::Synthesis),
-                ).map(Number);
+                    || arr[1].value().copied(),
+                )?;
 
                 // c
-                region.assign_advice(
+                let c_cell = region.assign_advice(
                     || "c",
                     self.config.c,
                     0,
-                    || c_val.ok_or(Error::Synthesis),
-                ).map(Number);
+                    || arr[2].value().copied(),
+                )?;
 
                 // d
-                region.assign_advice(
+                let d_cell = region.assign_advice(
                     || "d",
                     self.config.d,
                     0,
-                    || d_val.ok_or(Error::Synthesis),
-                ).map(Number);
+                    || arr[3].value().copied(),
+                )?;
 
-                Ok((arr[0].clone(), arr[1].clone(),arr[2].clone(),arr[3].clone()))
+                Ok((a_cell, b_cell, c_cell, d_cell))
             },
-        )
+        )?
         
     }
 
@@ -216,13 +228,9 @@ impl<F: FieldExt> Circuit<F> for BubSortCircuit<F> {
         mut layouter: impl Layouter<F>
     ) -> Result<(), Error> {
         let chip = BubSortChip::construct(config);
-        let (mut prev_a, mut prev_b, mut prev_c, mut prev_d) = chip.load_first_row(
+        let Ok((mut prev_a, mut prev_b, mut prev_c, mut prev_d)) = chip.load_first_row(
             layouter.namespace(|| "first row"),
-            self.a,
-            self.b,
-            self.c,
-            self.d,
-        )?;
+        );
         for _ in 0..4 {
             let (a, b, c, d) = chip.load_row(
                 layouter.namespace(|| "row"),
@@ -230,7 +238,7 @@ impl<F: FieldExt> Circuit<F> for BubSortCircuit<F> {
                 &prev_b,
                 &prev_c,
                 &prev_d,
-            )?;
+            );
             prev_a = a;
             prev_b = b;
             prev_c = c;
--- src/main_6.rs
+++ src/main_6.rs
@@ -2,8 +2,8 @@ use std::{marker::PhantomData};
 
 use halo2_proofs::{
     arithmetic::FieldExt,
-    circuit::{AssignedCell, Layouter, SimpleFloorPlanner},
-    plonk::{Advice, Circuit, Column, ConstraintSystem, Error, Instance, Selector},
+    circuit::*,
+    plonk::*,
     poly::Rotation,
 };
 
@@ -17,6 +17,10 @@ struct BubSortConfig {
     b: Column<Advice>,
     c: Column<Advice>,
     d: Column<Advice>,
+    a_0 : Column<Instance>,
+    b_0 : Column<Instance>,
+    c_0 : Column<Instance>,
+    d_0 : Column<Instance>,
     s: Selector,
 }
 
@@ -42,6 +46,10 @@ impl<F: FieldExt> BubSortChip<F> {
         let b = meta.advice_column();
         let c = meta.advice_column();
         let d = meta.advice_column();
+        let a_0 = meta.instance_column();
+        let b_0 = meta.instance_column();
+        let c_0 = meta.instance_column();
+        let d_0 = meta.instance_column();
         let s = meta.selector();
 
         // enable permutation checks for the following columns
@@ -49,11 +57,14 @@ impl<F: FieldExt> BubSortChip<F> {
         meta.enable_equality(b);
         meta.enable_equality(c);
         meta.enable_equality(d);
-        meta.enable_equality(i);
+        meta.enable_equality(a_0);
+        meta.enable_equality(b_0);
+        meta.enable_equality(c_0);
+        meta.enable_equality(d_0);
 
         // define the custom gate
         meta.create_gate("swap", |meta| {
-            let s = meta.query_selector(selector);
+            let s = meta.query_selector(s);
             let term_a = meta.query_advice(a, Rotation::cur());
             let term_b = meta.query_advice(b, Rotation::cur());
             let term_c = meta.query_advice(c, Rotation::cur());
@@ -66,18 +77,14 @@ impl<F: FieldExt> BubSortChip<F> {
         });
 
         BubSortConfig {
-            a, b, c, d, i, s,
+            a, b, c, d, a_0, b_0, c_0, d_0, s,
         }
     }
 
     fn load_first_row(
         &self,
         mut layouter: impl Layouter<F>,
-        a: F,
-        b: F,
-        c: F,
-        d: F,
-    ) -> Result<(Number<F>, Number<F>, Number<F>, Number<F>), Error> {
+    ) -> Result<(AssignedCell<F,F>, AssignedCell<F,F>, AssignedCell<F,F>, AssignedCell<F,F>), Error> {
         // load first row
         layouter.assign_region(
             || "first row",
@@ -85,33 +92,38 @@ impl<F: FieldExt> BubSortChip<F> {
                 // enable the selector
                 self.config.s.enable(&mut region, 0)?;
 
-                let a_num = region.assign_advice(
+
+                let a_num = region.assign_advice_from_instance(
                     || "a",
-                    self.config.a, // column a
+                    self.config.a_0, // column a
                     0, // rotation
-                    || Ok(a),
-                ).map(Number)?;
-
-                let b_num = region.assign_advice(
+                    self.config.a,
+                    0,
+                )?;
+                
+                let b_num = region.assign_advice_from_instance(
                     || "b",
-                    self.config.b, // column b
-                    0, // rotation
-                    || Ok(b),
-                ).map(Number)?;
-
-                let c_num = region.assign_advice(
+                    self.config.b_0, // column b
+                    1, // rotation
+                    self.config.b,
+                    0,
+                )?;
+                
+                let c_num = region.assign_advice_from_instance(
                     || "c",
-                    self.config.c, // column c
-                    0, // rotation
-                    || Ok(c),
-                ).map(Number)?;
+                    self.config.c_0, // column c
+                    2, // rotation
+                    self.config.c,
+                    0,
+                )?;
 
-                let d_num = region.assign_advice(
+                let d_num = region.assign_advice_from_instance(
                     || "d",
-                    self.config.d, // column c
-                    0, // rotation
-                    || Ok(d),
-                ).map(Number)?;
+                    self.config.d_0, // column c
+                    3, // rotation
+                    self.config.d,
+                    0,
+                )?;
 
                 Ok((a_num, b_num, c_num, d_num))
             },
@@ -121,70 +133,64 @@ impl<F: FieldExt> BubSortChip<F> {
     fn load_row(
         &self,
         mut layouter: impl Layouter<F>,
-        a: F,
-        b: F,
-        c: F,
-        d: F,
-    ) -> Result<(a_num, b_num, c_num, d_num), Error> {
+        prev_a: &AssignedCell<F,F>,
+        prev_b: &AssignedCell<F,F>,
+        prev_c: &AssignedCell<F,F>,
+        prev_d: &AssignedCell<F,F>,
+    ) -> (AssignedCell<F,F>, AssignedCell<F,F>, AssignedCell<F,F>, AssignedCell<F,F>) {
         // do a compare & swap and assign a new region
         let mut arr = [prev_a.clone(), prev_b.clone(), prev_c.clone(), prev_d.clone()];
         for idx in 0..3 {
             let mut a = arr[idx];
-            let mut b = arr[idx+1]
+            let mut b = arr[idx+1];
 
-            if Some(true) == a.0.value().and_then(|a| b.0.value().map(|b| *a > *b)) {
-                arr.swap(arr[idx], arr[idx+1])
+            if a.value() > b.value() {
+                arr.swap(idx, idx+1)
             }
         }
 
 
         layouter.assign_region(
-            || "row",
+            || "next row",
             |mut region| {
                 // enable the selector
                 self.config.s.enable(&mut region, 0)?;
 
-                // copy the cell from previous row
-                let a_val = arr[0].0.value();
-                let b_val = arr[1].0.value();
-                let c_val = arr[2].0.value();
-                let d_val = arr[3].0.value();
-
                 // a
-                region.assign_advice(
+                let a_cell = region.assign_advice(
                     || "a",
                     self.config.a,
                     0,
-                    || a_val.ok_or(Error::Synthesis),
-                ).map(Number)
+                    || arr[0].value(),
+                )?;
 
                 // b
-                region.assign_advice(
+                let b_cell = region.assign_advice(
                     || "b",
                     self.config.b,
                     0,
-                    || b_val.ok_or(Error::Synthesis),
-                ).map(Number)
+                    || arr[1].value(),
+                )?;
 
                 // c
-                region.assign_advice(
+                let c_cell =region.assign_advice(
                     || "c",
                     self.config.c,
                     0,
-                    || c_val.ok_or(Error::Synthesis),
-                ).map(Number)
+                    || arr[2].value(),
+                )?;
 
                 // d
-                region.assign_advice(
+                let d_cell = region.assign_advice(
                     || "d",
                     self.config.d,
                     0,
-                    || d_val.ok_or(Error::Synthesis),
-                ).map(Number)
+                    || arr[3].value(),
+                )?;
 
-                Ok((arr[0].clone(), arr[1].clone(),arr[2].clone(),arr[3].clone()))
+                Ok((a_cell, b_cell, c_cell, d_cell))
             },
-        )
+        )?
         
     }
 
@@ -218,11 +224,7 @@ impl<F: FieldExt> Circuit<F> for BubSortCircuit<F> {
         let chip = BubSortChip::construct(config);
         let (mut prev_a, mut prev_b, mut prev_c, mut prev_d) = chip.load_first_row(
             layouter.namespace(|| "first row"),
-            self.a,
-            self.b,
-            self.c,
-            self.d,
-        )?;
+        );
         for _ in 0..4 {
             let (a, b, c, d) = chip.load_row(
                 layouter.namespace(|| "row"),
@@ -230,7 +232,7 @@ impl<F: FieldExt> Circuit<F> for BubSortCircuit<F> {
                 &prev_b,
                 &prev_c,
                 &prev_d,
-            )?;
+            );
             prev_a = a;
             prev_b = b;
             prev_c = c;
@@ -242,13 +244,12 @@ impl<F: FieldExt> Circuit<F> for BubSortCircuit<F> {
 
 
 fn main() {
-    use halo2_proofs::{dev::MockProver, pairing::bn256::Fr as Fp};
-
+    use halo2_proofs::{dev::MockProver, pasta::Fp};
     // Prepare the private and public inputs to the circuit!
-    let arr = [13,324,3,88]
+    let arr = [13,324,3,88];
 
     // Instantiate the circuit with the private inputs.
-    let circuit = FiboCircuit {
+    let circuit = BubSortCircuit {
         a: Fp::from(arr[0]),
         b: Fp::from(arr[1]),
         c: Fp::from(arr[2]),
@@ -260,7 +261,7 @@ fn main() {
     let k = 4;
 
     // Given the correct public input, our circuit will verify.
-    let prover = MockProver::run(k, &circuit, _).unwrap();
+    let prover = MockProver::run(k, &circuit, vec![]).unwrap();
     assert_eq!(prover.verify(), Ok(()));
 }
 
